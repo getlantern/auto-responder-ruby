6999b8c5888ee4ad9880065727180c6b1207c4b7
fffw <fffw@getlantern.org>
2015-07-30 23:45:55 +0800

    inbox fetcher adds addr to mailchimp directly

--- inbox_fetcher.rb
+++ inbox_fetcher.rb
@@ -2,16 +2,45 @@
 require "rubygems"
 require "bundler/setup"
 require "mailman"
+require 'mailchimp'
 
+EMAIL_RESPONDER_ACCOUNT = ENV["EMAIL_RESPONDER_ACCOUNT"] or abort("no environment variable EMAIL_RESPONDER_ACCOUNT")
+EMAIL_RESPONDER_PASSWORD = ENV["EMAIL_RESPONDER_PASSWORD"] or abort("no environment variable EMAIL_RESPONDER_PASSWORD")
+MAILCHIMP_API_KEY = ENV["MAILCHIMP_API_KEY"] or abort("no environment variable MAILCHIMP_API_KEY")
+MAILCHIMP_LIST_ID = ENV["MAILCHIMP_LIST_ID"] or abort("no environment variable MAILCHIMP_LIST_ID")
 
 Mailman.config.poll_interval = 1
 
 Mailman.config.imap = {
   server: 'imap.gmail.com', port: 993, ssl: true,
-  username: ENV['EMAIL_RESPONDER_ACCOUNT'],
-  password: ENV['EMAIL_RESPONDER_PASSWORD'],
+  username: EMAIL_RESPONDER_ACCOUNT,
+  password: EMAIL_RESPONDER_PASSWORD,
   filter: 'ALL'
 }
+
+def add_to_mailchimp(name, addr)
+  begin
+    merge_vars = nil
+    if name then
+      fname, lname = name.split
+      merge_vars = {
+        'FNAME' => fname,
+        'LNAME' => lname
+      }
+    end
+    mailchimp = Mailchimp::API.new(MAILCHIMP_API_KEY)
+    mailchimp.lists.subscribe(MAILCHIMP_LIST_ID,
+                              { "email" => addr },
+                              merge_vars,
+                              'html', # email_type
+                              false, # double_optin
+                              true) # update_existing
+    Mailman.logger.info "Added #{addr} to mailchimp"
+  rescue Exception => e
+    Mailman.logger.error "Exception occurred while add #{addr} to mailchimp"
+    Mailman.logger.error [e, *e.backtrace].join("\n")
+  end
+end
 
 Mailman::Application.run do
   from(/no[-_]*reply/) do
@@ -27,11 +56,10 @@ Mailman::Application.run do
 
   default do
     begin
-      open('inbox.csv', 'a') do |f|
-        f.print(message["From"].address_list.addresses[0].display_name, ',', message.from[0], ',', message.date.to_s, "\n")
-      end
+      name = message["From"].address_list.addresses[0].display_name
+      add_to_mailchimp(name, message.from[0])
     rescue Exception => e
-      Mailman.logger.error "Exception occurred while receiving message:\n#{message}"
+      Mailman.logger.error "Exception occurred while processing message:\n#{message}"
       Mailman.logger.error [e, *e.backtrace].join("\n")
     end
   end
