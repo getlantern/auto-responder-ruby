11e9df72039e2ecfdf9f3297f913cacf0fb1f2d4
joesis <joesis@getlantern.org>
2018-01-08 05:22:07 -0800

    hit mandrill v3 api directly

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 # A sample Gemfile
 ruby '2.5.0'
 source "https://rubygems.org"
-gem 'mailchimp-api'
 gem 'mandrill-api'
 gem "mailman"
 gem "mail"
--- lib/mailchimp.rb
+++ lib/mailchimp.rb
@@ -1,5 +1,3 @@
-require 'mailchimp'
-
 MAILCHIMP_API_KEY = ENV["MAILCHIMP_API_KEY"] or abort("no environment variable MAILCHIMP_API_KEY")
 MAILCHIMP_LIST_ID = ENV["MAILCHIMP_LIST_ID"] or abort("no environment variable MAILCHIMP_LIST_ID")
 MAILCHIMP_LANG = ENV["MAILCHIMP_LANG"] or abort("no environment variable MAILCHIMP_LANG")
@@ -15,14 +13,23 @@ def add_to_mailchimp(name, addr)
         'mc_language' => MAILCHIMP_LANG
       }
     end
-    mailchimp = Mailchimp::API.new(MAILCHIMP_API_KEY)
-    mailchimp.lists.subscribe(MAILCHIMP_LIST_ID,
-                              { "email" => addr },
-                              merge_vars,
-                              'html', # email_type
-                              false, # double_optin
-                              true) # update_existing
-    Mailman.logger.info "Added #{addr} to mailchimp"
+    uri = URI("https://us2.api.mailchimp.com/3.0/lists/#{MAILCHIMP_LIST_ID}/members")
+    req = Net::HTTP::Post.new(uri)
+    req.basic_auth "user", MAILCHIMP_API_KEY
+    req.body = {email_address: addr, email_type: 'html', status: 'subscribed', merge_fields: merge_vars}.to_json
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
+      http.request(req)
+    }
+    if res.code == "200" then
+      Mailman.logger.info "Added #{addr} to mailchimp"
+    else
+      error = JSON.parse(res.body)
+      if error['title'] == 'Member Exists' then
+        Mailman.logger.info "#{addr} is already an subscriber on mailchimp"
+      else
+        Mailman.logger.info "Fail to add #{addr} to mailchimp: #{error['status']} #{error['title']} - #{error['detail']}"
+      end
+    end
   rescue Exception => e
     Mailman.logger.info "Exception occurred while add #{addr} to mailchimp"
     Mailman.logger.info e
